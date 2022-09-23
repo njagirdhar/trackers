004855f66bc7de5e78fa4d4083264c43a7e2152a
Naveen Jagirdhar <48186392+Naveen-Jagirdhar@users.noreply.github.com>
2022-09-24 00:36:51 +0530

    initial commit

---
+++ src/main/java/com/app/trackers/TrackersApplication.java
@@ -0,0 +1,13 @@
+package com.app.trackers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/trackers/TrackersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.trackers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
