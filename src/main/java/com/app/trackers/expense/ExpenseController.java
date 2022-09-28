3cd03c7b85b7438d57123558c2d9006c8b7d760d
Naveen Jagirdhar <48186392+Naveen-Jagirdhar@users.noreply.github.com>
2022-09-29 02:26:34 +0530

    Update ExpenseController.java

--- src/main/java/com/app/trackers/expense/ExpenseController.java
+++ src/main/java/com/app/trackers/expense/ExpenseController.java
@@ -5,6 +5,7 @@ import com.app.trackers.expense.service.ExpenseServices;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/api/v1/expenses")
 @Slf4j
+@CrossOrigin
 public class ExpenseController {
 
     private final ExpenseServices expenseServices;
